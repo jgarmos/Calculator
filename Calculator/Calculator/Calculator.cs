580979b09220a2c7f7b23c7a5e43cd79117e61ea
javiergarcia <javiergarcia@OFFICE194>
2016-11-08 09:27:33 +0000

    Unit tests

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int arg1, int arg2)
+        {
+            return 4;
+        }
+    }
+}
---
+++ Calculator/CalculatorTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using  CalculatorTdd;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Add()
+        {
+            Calculator.Calculator calculator = new Calculator.Calculator();
+            int result = calculator.Add(2, 2);
+            Assert.AreEqual(4, result);
+        }
+        [TestMethod]
+        public void AddWithDifferentsArguments()
+        {
+            Calculator.Calculator calculator = new Calculator.Calculator();
+            int result = calculator.Add(2, 5);
+            Assert.AreEqual(7, result);
+        }
+    }
+}
